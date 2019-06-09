c5e58c79f34cbe2980f0fc090b6403d871f523ee
Ryandi Tjia <ryandi.tjia@me.com>
2019-06-09 15:40:35 +0700

    Override sanitize.css instead of forking

--- src/config/global-styles.ts
+++ src/config/global-styles.ts
@@ -1,10 +1,27 @@
 import '@/assets/fonts/loader.css'
-import '@/assets/my-sanitize.css'
 import { appendSystemFonts } from '@/utils/mixins'
 import { css } from '@emotion/core'
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
 
 export default css`
   html {
     font-family: ${appendSystemFonts('Futura')};
+    cursor: initial;
+  }
+
+  abbr[title] {
+    cursor: help;
+  }
+
+  summary {
+    cursor: pointer;
+  }
+
+  [type='button']:not(:disabled),
+  [type='reset']:not(:disabled),
+  [type='submit']:not(:disabled),
+  button:not(:disabled) {
+    cursor: pointer;
   }
 `
